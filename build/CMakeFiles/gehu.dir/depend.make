c88ccee016eeb8872e966a3d18928c027d894696
Navneet Pathak <148359340+navneetpathak1@users.noreply.github.com>
2025-05-30 11:35:01 +0530

    deleting

--- build/CMakeFiles/gehu.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for gehu.
-# This may be replaced when dependencies are built.
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/navneet/project/gehu_project/test
-# Build directory: /home/navneet/project/gehu_project/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gehu_test "/home/navneet/project/gehu_project/build/test/gehu_test")
-set_tests_properties(gehu_test PROPERTIES  _BACKTRACE_TRIPLES "/home/navneet/project/gehu_project/test/CMakeLists.txt;33;add_test;/home/navneet/project/gehu_project/test/CMakeLists.txt;0;")
